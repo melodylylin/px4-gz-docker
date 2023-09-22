fd2a2a85a2dde85d9a02822a618795d2c3117113
Li-Yu Lin <liyu8561501@gmail.com>
2023-09-22 10:06:50 -0400

    add offboard and change src

--- get_src.sh
+++ get_src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -d ./work/px4 ] ; then
     cd ./work
-    git clone git@github.com:PX4/PX4-Autopilot px4
+    git clone git@github.com:melodylylin/PX4-Autopilot px4
     cd px4
     git tag v1.14.0-beta2
     cd ../..
@@ -11,6 +11,7 @@ if [ ! -d ./work/ros2_ws/src ] ; then
     mkdir -p ./work/ros2_ws/src
     cd work/ros2_ws/src
     git clone git@github.com:PX4/px4_msgs.git
+    git clone git@github.com:melodylylin/px4-offboard.git
     git clone -b humble git@github.com:gazebosim/ros_gz.git
 fi
 
