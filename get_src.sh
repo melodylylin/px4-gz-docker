40b514d6f8a489b5c69a972478c82c4ef0d5bc7a
Li-Yu Lin <liyu8561501@gmail.com>
2023-09-22 09:35:30 -0400

    add offboard to workspace

--- get_src.sh
+++ get_src.sh
@@ -11,6 +11,7 @@ if [ ! -d ./work/ros2_ws/src ] ; then
     mkdir -p ./work/ros2_ws/src
     cd work/ros2_ws/src
     git clone git@github.com:PX4/px4_msgs.git
+    git clone git@github.com:Jaeyoung-Lim/px4-offboard.git
     git clone -b humble git@github.com:gazebosim/ros_gz.git
 fi
 
