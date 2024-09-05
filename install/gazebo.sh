25bcc02b2b242e93943b7223de8a77aee85135a0
Li-Yu Lin <liyu8561501@gmail.com>
2024-09-04 21:36:08 -0400

    update gz version to harmonic

--- install/gazebo.sh
+++ install/gazebo.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-GAZEBO_VERSION="garden"
+GAZEBO_VERSION="harmonic"
 
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
